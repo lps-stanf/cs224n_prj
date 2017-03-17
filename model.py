c41e5a0c29bc9141d93ac255f02f2e24e32c27ef
Boris Kovalenko <bkovalenko@nvidia.com>
2017-03-17 20:41:06 +0300

    check new name

--- model.py
+++ model.py
@@ -24,6 +24,7 @@ from model_checkpoints import MyModelCheckpoint
 adam = keras.optimizers.Adam(lr=0.0002, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
 nadam = keras.optimizers.Nadam(lr=0.0005, beta_1=0.9, beta_2=0.999, epsilon=1e-08, schedule_decay=0.004)
 
+
 def create_image_model(images_shape, repeat_count):
     inputs = Input(shape=images_shape)
 
