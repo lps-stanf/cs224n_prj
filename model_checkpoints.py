7b539cb1c68bdc970b11356fa4a2a55852cba33a
Boris Kovalenko <bkovalenko@nvidia.com>
2017-03-17 21:18:21 +0300

    add model_id

--- model_checkpoints.py
+++ model_checkpoints.py
@@ -4,10 +4,11 @@ import keras
 
 
 class MyModelCheckpoint(keras.callbacks.Callback):
-    def __init__(self, dir, weights_name, epoch_period = 1):
+    def __init__(self, dir, weights_name, epoch_period=1, model_id=None):
         self.path_begin = os.path.join(dir, weights_name)
+        self.model_id = model_id
         self.period = max(1, epoch_period)
-        self.log_file = open(os.path.join(dir, 'epoch.log'), 'w')
+        self.log_file = open(os.path.join(dir, 'epoch_history_{0}.log'.format(self.model_id)), 'w')
 
     def on_epoch_end(self, epoch, logs=None):
         # we want 1-indexed epoch in output
@@ -18,7 +19,7 @@ class MyModelCheckpoint(keras.callbacks.Callback):
             self.log_file.write('Epoch {0}: Train {1}\n'.format(epoch, logs['loss']))
 
         if epoch % self.period == 0:
-            filename = '{}_{:04}.hdf5'.format(self.path_begin, epoch)
+            filename = '{0}_{:04}_{2}.hdf5'.format(self.path_begin, epoch, self.model_id)
             self.model.save_weights(filename)
 
     def on_train_end(self, logs=None):
