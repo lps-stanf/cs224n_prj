74eec6483bde25b1569d8d02471be6e3cdaf1adb
Peter <plapko@stanford.edu>
2017-03-18 05:28:26 +0300

    Added reading of settings from specified module

--- settings_keeper.py
+++ settings_keeper.py
@@ -25,22 +25,34 @@ class SettingsKeeper(object):
         val = dict.get(key, default)
         self._add_key_value(key, val, type)
 
-    def add_ini_file(self, ini_file_path):
-        config = ConfigParser(allow_no_value=True)
-        with open(ini_file_path, 'r') as f:
-            config.read_file(f)
+    def _add_ini_file_section(self, config_parser, section_name, require_provided_section=True):
+        sections_list = config_parser.sections()
+        if section_name not in sections_list:
+            if not require_provided_section:
+                return
+            raise RuntimeError('No required section in config file: "{0}"'.format(section_name))
 
-        for section in config.sections():
-            for option_key in config.options(section):
-                option_value = config.get(section, option_key)
-                option_key_list = option_key.split()
-                if len(option_key_list) > 2:
-                    raise ValueError('Error in config, key is too long "{}"'.format(option_key))
-                type = None
-                if len(option_key_list) == 2:
-                    type = locate(option_key_list[0])
-                    option_key_list.pop(0)
-                self._add_key_value(option_key_list[0], option_value, type)
+        for option_key in config_parser.options(section_name):
+            option_value = config_parser.get(section_name, option_key)
+            option_key_list = option_key.split()
+            if len(option_key_list) > 2:
+                raise ValueError('Error in config, key is too long "{}"'.format(option_key))
+            type = None
+            if len(option_key_list) == 2:
+                type = locate(option_key_list[0])
+                option_key_list.pop(0)
+            self._add_key_value(option_key_list[0], option_value, type)
+
+    def add_ini_file(self, ini_file_path, section_name=None, require_provided_section=True):
+        config_parser = ConfigParser(allow_no_value=True)
+        with open(ini_file_path, 'r') as f:
+            config_parser.read_file(f)
+
+        if section_name is not None:
+            self._add_ini_file_section(config_parser, section_name, require_provided_section)
+        else:
+            for section in config_parser.sections():
+                self._add_ini_file_section(config_parser, section)
 
     def add_parsed_arguments(self, args: argparse.Namespace):
         for prop, val in vars(args).items():
