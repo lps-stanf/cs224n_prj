f28e46410bc70ae371d45ca62c25ac69da091e1d
ViktorM <victor.makoviychuk@gmail.com>
2017-03-19 04:30:06 -0700

    Added stacked GRU model.

--- model.py
+++ model.py
@@ -102,6 +102,30 @@ def create_default_model(images_shape, dict_size, sentence_len, settings, pretra
     combined_model.compile(loss='sparse_categorical_crossentropy', optimizer=create_optimizer(settings))
     return combined_model
 
+def create_GRU_stack_model(images_shape, dict_size, sentence_len, settings, pretrained_emb):
+    # input (None, 224, 224, 3), outputs (None, sentence_len, 512)
+    image_model = create_image_model(images_shape, sentence_len)
+
+    # outputs (None, sentence_len, 128)
+    sentence_model = create_sentence_model(dict_size, sentence_len, pretrained_emb)
+
+    combined_model = Sequential()
+    combined_model.add(Merge([image_model, sentence_model], mode='concat', concat_axis=-1))
+    combined_model.add(GRU(192, return_sequences = True, dropout_U = 0.15, dropout_W = 0.25))
+
+    combined_model2 = Sequential()
+    combined_model2.add(Merge([image_model, combined_model], mode='concat', concat_axis=-1))
+    combined_model2.add(GRU(256, return_sequences = False, dropout_U = 0.1, dropout_W = 0.2))
+
+    combined_model.add(Dense(dict_size))
+    combined_model.add(Activation('softmax'))
+
+    # input words are 1-indexed and 0 index is used for masking!
+    # but result words are 0-indexed and will go into [0, ..., dict_size-1] !!!
+
+    combined_model.compile(loss='sparse_categorical_crossentropy', optimizer=create_optimizer(settings))
+    return combined_model
+
 
 def create_lstm_nadam_model(images_shape, dict_size, sentence_len, settings, pretrained_emb):
     # input (None, 224, 224, 3), outputs (None, sentence_len, 512)
