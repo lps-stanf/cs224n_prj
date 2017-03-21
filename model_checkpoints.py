fc6e44438480d0de2f76241f25fdd0ed4e877686
Peter <plapko@stanford.edu>
2017-03-22 01:01:48 +0300

    Changed output filename format

--- model_checkpoints.py
+++ model_checkpoints.py
@@ -27,13 +27,18 @@ class MyModelCheckpoint(keras.callbacks.Callback):
 
 
 class BestModelCheckpoint(keras.callbacks.Callback):
-    def __init__(self, dir, weights_name, epoch_period=1, model_id=None):
-        self.path_begin = os.path.join(dir, weights_name)
+    def __init__(self, dir, model_name, epoch_period=1, model_id=None):
         self.model_id = model_id
         self.period = max(1, epoch_period)
-        self.log_file = open(os.path.join(dir, 'epoch_history_{0}.log'.format(self.model_id)), 'w')
         self.best_loss = 1.0e6
         self.best_epoch = -1
+        self.model_name = model_name
+
+        self.out_dir = os.path.join(dir, model_name)
+        if not os.path.isdir(self.out_dir):
+            os.makedirs(self.out_dir)
+
+        self.log_file = open(os.path.join(dir, '{}__{}.log'.format(self.model_name, self.model_id)), 'w')
 
     def on_epoch_end(self, epoch, logs=None):
         # we want 1-indexed epoch in output
@@ -43,7 +48,8 @@ class BestModelCheckpoint(keras.callbacks.Callback):
                 self.best_loss = logs['val_loss']
                 self.best_epoch = epoch
                 self.log_file.write('New best result achieved!\n')
-                filename = 'best_{}_{:03}.hdf5'.format(self.model_id, epoch)
+                filename = os.path.join(self.out_dir,
+                                        'best_{}__{:03}__{}.hdf5'.format(self.model_name, epoch, self.model_id))
                 self.model.save_weights(filename)
             self.log_file.write('Epoch {0}\tTrain {1}\t Val {2}\n'.format(epoch, logs['loss'], logs['val_loss']))
 
@@ -51,7 +57,7 @@ class BestModelCheckpoint(keras.callbacks.Callback):
             self.log_file.write('Epoch {0}\t Train {1}\n'.format(epoch, logs['loss']))
 
         if epoch % self.period == 0:
-            filename = '{}_{:03}_{}.hdf5'.format(self.path_begin, epoch, self.model_id)
+            filename = os.path.join(self.out_dir, '{}__{:03}__{}.hdf5'.format(self.model_name, epoch, self.model_id))
             self.model.save_weights(filename)
 
     def on_train_end(self, logs=None):
